53502767a40d8c36c97d292d6e1272cb0f029565
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2024-02-08 23:52:57 +1300

    make working script

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,33 @@
 use core::panic;
 use std::{
-    io::stdin,
+    env::current_dir,
+    io::{stdin, BufRead, BufReader},
     process::{Command, Stdio},
 };
 
 fn main() {
     println!("Welcome to auto reflector!");
     backup_mirror_list();
-    grab_mirror_list();
+    match grab_mirror_list() {
+        Ok(_x) => println!("Updated mirror list successfully"),
+        Err(x) => println!("{x}"),
+    }
 }
 
 fn backup_mirror_list() {
-    let command = "sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup";
-    let mut child = Command::new(command)
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .spawn()
+    let command = "sudo";
+    let arg = "/etc/pacman.d/mirrorlist";
+    let arg2 = "/etc/pacman.d/mirrorlist.backup";
+    let current_dir = current_dir().unwrap();
+    let dir_path = current_dir.to_str().unwrap();
+    let child = Command::new(command)
+        .arg("cp")
+        .arg(arg)
+        .arg(arg2)
+        .current_dir(dir_path)
+        .output()
         .expect("Failed to execute command");
-    let status = child.wait().expect("Failed to wait for command");
-    if status.success() {
+    if child.status.success() {
         println!("Mirror list backup successful");
     } else {
         println!("Could not backup mirror list. Continue anyway? (Y/n)");
@@ -39,13 +48,26 @@ fn backup_mirror_list() {
     }
 }
 
-fn grab_mirror_list() {
-    let command = "sudo reflector --verbose --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist";
-    let output = Command::new(command)
-        .output()
-        .expect("Failed to execute reflector command...");
-    if !output.status.success() {
-        println!("Failed to retrieve mirror list");
+fn grab_mirror_list() -> std::io::Result<()> {
+    let command = "sudo";
+    let mut output = Command::new(command)
+        .arg("reflector")
+        .arg("--verbose")
+        .arg("--latest")
+        .arg("10")
+        .arg("--protocol")
+        .arg("https")
+        .arg("--sort")
+        .arg("rate")
+        .arg("--save")
+        .arg("/etc/pacman.d/mirrorlist")
+        .stdout(Stdio::piped())
+        .spawn()?;
+    let stdout = output.stdout.as_mut().expect("Failed to open stdout");
+    let reader = BufReader::new(stdout);
+    for line in reader.lines() {
+        println!("{}", line?);
     }
-    println!("{}", String::from_utf8_lossy(&output.stdout));
+    output.wait()?;
+    Ok(())
 }
